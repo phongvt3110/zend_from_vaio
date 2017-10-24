c6f30af54a982a1c4e0a4f6b633102b18b776960
ex-phongvt <ex.phongvt@nextbeat.net>
2017-10-24 16:22:33 +0700

    update code

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,16 @@
  */
 
 return [
-    // ...
+    'db' => [
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=zend3;host=localhost',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ]
+    ],
+    'service_manager' => [
+        'factories' => [
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+        ]
+    ]
 ];
